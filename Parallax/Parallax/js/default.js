0cf48b70640feb9a6e866218a038d318c12e1d48
Dan Kass <daniel.kass@tufts.edu>
2012-08-17 10:01:29 -0700

    win8 experiments and studies

---
+++ Parallax/Parallax/js/default.js
@@ -0,0 +1,45 @@
+﻿$(function () {
+
+    var totalWidth = 1366,
+        margin = 100;
+
+
+   /* $('img').each(function (index) {
+
+        var $img = $(this),
+            imgX = Math.round($img.position().left),
+            progress = (imgX + $img.width()) / (totalWidth + $img.width());
+
+        if (progress > 1.0) progress = 1.0;
+        else if (progress < 0.0) progress = 0.0;
+
+        var marginLeft = -Math.round(progress * margin),
+            marginRight = -(margin + marginLeft);
+
+        $img.css({
+            'margin-right': marginRight + 'px',
+            'margin-left': marginLeft + 'px',
+        });
+    });*/
+    
+    $('ul.old').scroll(function () {
+
+        $('ul.old img').each(function (index) {
+
+            var $img = $(this),
+                imgX = Math.round($img.position().left),
+                progress = (imgX + $img.width()) / (totalWidth + $img.width());
+
+            if (progress > 1.0) progress = 1.0;
+            else if (progress < 0.0) progress = 0.0;
+
+            var marginLeft = - Math.round(progress * margin),
+                marginRight = - (margin + marginLeft);       
+
+            $img.css({
+                'margin-right': marginRight  + 'px',
+                'margin-left': marginLeft + 'px',
+            });       
+        });
+    });
+});
